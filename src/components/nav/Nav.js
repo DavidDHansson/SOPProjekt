0914d0fdc0716acd23da41cf29502b89db685c16
David Hansson <Hansson.d.david@gmail.com>
2020-11-09 08:55:57 +0100

    Nav bar work

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffects } from 'react'
 import "./Nav.css";
 
 export default function Nav() {
+
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            const top = window.scrollY > window.screen.height;
+            const nav = document.getElementById("nav");
+            if (nav) {
+                if(!top) {
+                    nav.classList.add("nav-extended");
+                    nav.classList.remove("nav-not-extended");
+                    console.log("open");
+                } else {
+                    nav.classList.add("nav-not-extended");
+                    nav.classList.remove("nav-extended");
+                    console.log("close");
+                }
+            }
+        });
+
+        return () => {
+            document.removeEventListener("scroll", () => { });
+        }
+    }, []);
+
     return (
-        <div className="nav-flex-wrapper">
+        <div className="nav-flex-wrapper" id="nav">
 
             <div className="nav-main-wrapper">
                 <div>LOGO</div>
