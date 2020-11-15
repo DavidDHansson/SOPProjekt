b0e33fa959732b5eb833c83580aaa0903e47adf6
David Hansson <Hansson.d.david@gmail.com>
2020-11-15 16:32:00 +0100

    Auth0 logic and dry code for ticket

--- src/screens/user/LoginOptionTicket.js
+++ src/screens/user/LoginOptionTicket.js
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import "./User.css";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 import * as Reveal from 'react-reveal/Fade';
 import * as RevealButton from 'react-reveal/Jump';
@@ -17,23 +20,67 @@ export default function LoginOptionTicket(props) {
                 <div className="user-ticket-desc">
                     {viewModel?.message ?? ""}
                 </div>
-                <Bottom index={index}/>
+                <Bottom index={index} type={viewModel.type}/>
             </div>
         </Reveal>
     )
 }
 
 function Bottom(props) {
+
+    const { isAuthenticated: auth0IsAuthenticated, loginWithRedirect: auth0LoginWithRedirect, logout: auth0Logout } = useAuth0();
+
+    function login() {
+        switch(props.type) {
+            case "auth0":
+                auth0LoginWithRedirect();
+                break;
+            case "firebase":
+                break;
+            case "custom":
+                break;
+            default:
+                break;
+        }
+    }
+    
+    function logout() {
+        switch(props.type) {
+            case "auth0":
+                auth0Logout();
+                break;
+            case "firebase":
+                break;
+            case "custom":
+                break;
+            default:
+                break;
+        }
+    }
+
+    function isUserAuthenticated() {
+        switch(props.type) {
+            case "auth0":
+                return auth0IsAuthenticated
+            case "firebase":
+                break;
+            case "custom":
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <div style={{ marginTop: "auto" }} className="user-ticket-bottom-wrapper">
             <RevealButton delay={props.index * 400}>
-                <div className="user-ticket-button">
+                <div className="user-ticket-button" onClick={() => login() }>
                     {"Start"}
                 </div>
             </RevealButton>
-            <span className="user-ticket-logout">
+            {isUserAuthenticated() && (<span className="user-ticket-logout" onClick={() => logout() }>
                 {"Log ud"}
-            </span>
+            </span>)}
         </div>
     );
 }
