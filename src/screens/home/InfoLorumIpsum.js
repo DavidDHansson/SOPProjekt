32db44af68c8415c9ef6f7ee4708e53beb0be997
David Hansson <Hansson.d.david@gmail.com>
2020-11-17 17:16:38 +0100

    Color change and react reveal

--- src/screens/home/InfoLorumIpsum.js
+++ src/screens/home/InfoLorumIpsum.js
@@ -1,41 +1,50 @@
 import React from 'react'
+
 import svg1 from "./assets/undraw_security_o890.svg";
 import svg2 from "./assets/undraw_authentication_fsn5.svg";
 import svg3 from "./assets/undraw_posting_photo_v65l.svg";
+
+import * as Reveal from 'react-reveal/Reveal';
 
 export default function InfoLorumIpsum(props) {
 
     if (props.index == 0) {
         return (
             <div className="home-info-main-wrapper home-info-color5">
-                <div className="home-info-wrapper">
-                    <img src={svg1} className="home-info-image" />
-                    <div className="home-info-text-wrapper">
-                        <p className="home-info-title">Lorem ipsum dolor</p>
-                        <p className="home-info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non nisi et augue bibendum lobortis nec et diam. In blandit sagittis turpis, nec molestie lorem auctor eu. Morbi fermentum mi libero, sit amet elementum nisl volutpat at. Donec vehicula enim vitae enim placerat eleifend.</p>
+                <Reveal>
+                    <div className="home-info-wrapper">
+                        <img src={svg1} className="home-info-image" />
+                        <div className="home-info-text-wrapper">
+                            <p className="home-info-title">Lorem ipsum dolor</p>
+                            <p className="home-info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non nisi et augue bibendum lobortis nec et diam. In blandit sagittis turpis, nec molestie lorem auctor eu. Morbi fermentum mi libero, sit amet elementum nisl volutpat at. Donec vehicula enim vitae enim placerat eleifend.</p>
+                        </div>
                     </div>
-                </div>
-                <div className="home-info-wrapper">
-                    <div className="home-info-text-wrapper">
-                        <p className="home-info-title">Morbi eleifend lacus</p>
-                        <p className="home-info-text">Morbi eleifend lacus quis sapien gravida tincidunt vestibulum ut ligula. Vivamus quis ex tellus. Mauris elementum nisi sit amet erat scelerisque, hendrerit ullamcorper odio tempor. Cras a mauris nisl. Ut sagittis aliquet dui sed congue. Nam aliquam eget orci non ornare. Fusce nec turpis elementum, mattis nisi id, porttitor ipsum. Nunc semper sodales mi mollis fringilla. Pellentesque in odio at lorem imperdiet bibendum ac ac nulla.</p>
+                </Reveal>
+                <Reveal>
+                    <div className="home-info-wrapper">
+                        <div className="home-info-text-wrapper">
+                            <p className="home-info-title">Morbi eleifend lacus</p>
+                            <p className="home-info-text">Morbi eleifend lacus quis sapien gravida tincidunt vestibulum ut ligula. Vivamus quis ex tellus. Mauris elementum nisi sit amet erat scelerisque, hendrerit ullamcorper odio tempor. Cras a mauris nisl. Ut sagittis aliquet dui sed congue. Nam aliquam eget orci non ornare. Fusce nec turpis elementum, mattis nisi id, porttitor ipsum. Nunc semper sodales mi mollis fringilla. Pellentesque in odio at lorem imperdiet bibendum ac ac nulla.</p>
+                        </div>
+                        <img src={svg2} className="home-info-image" />
                     </div>
-                    <img src={svg2} className="home-info-image" />
-                </div>
-                <Divider2 index={props.index}/>
+                </Reveal>
+                <Divider2 index={props.index} />
             </div>
         )
     } else if (props.index == 1) {
         return (
             <div className="home-info-main-wrapper home-info-color4">
-                <div className="home-info-wrapper">
-                    <img src={svg3} className="home-info-image" />
-                    <div className="home-info-text-wrapper">
-                        <p className="home-info-title">Lorem ipsum dolor</p>
-                        <p className="home-info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non nisi et augue bibendum lobortis nec et diam. In blandit sagittis turpis, nec molestie lorem auctor eu. Morbi fermentum mi libero, sit amet elementum nisl volutpat at. Donec vehicula enim vitae enim placerat eleifend.</p>
+                <Reveal>
+                    <div className="home-info-wrapper">
+                        <img src={svg3} className="home-info-image" />
+                        <div className="home-info-text-wrapper">
+                            <p className="home-info-title">Lorem ipsum dolor</p>
+                            <p className="home-info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non nisi et augue bibendum lobortis nec et diam. In blandit sagittis turpis, nec molestie lorem auctor eu. Morbi fermentum mi libero, sit amet elementum nisl volutpat at. Donec vehicula enim vitae enim placerat eleifend.</p>
+                        </div>
                     </div>
-                </div>
-                <Divider2 index={props.index}/>
+                </Reveal>
+                <Divider2 index={props.index} />
             </div>
         );
     } else {
--- src/screens/home/Quotes.js
+++ src/screens/home/Quotes.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
+import * as Reveal from 'react-reveal/Zoom';
+
 export default function Quotes() {
     return (
         <div className="home-quotes-main-wrapper">
             <div className="home-quotes-wrapper">
-                <div>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi varius, turpis vitae molestie sodales, ligula sapien scelerisque nunc, in fermentum risus velit quis est. Donec gravida lorem felis, ut pellentesque leo luctus eu. Praesent sit amet ultricies sem. Praesent sit amet viverra nunc."</p>
-                    <p>- Eve Salinas</p>
-                </div>
-                <div>
-                    <p>"Etiam lacinia in massa id malesuada. Fusce vitae ipsum egestas, pellentesque leo ac, malesuada augue. Sed id dictum est. Curabitur vitae magna congue, porttitor tellus quis, ullamcorper arcu. Curabitur ornare euismod vulputate. Praesent auctor ullamcorper dolor, vel malesuada nunc sollicitudin at."</p>
-                    <p>- Daria Farrington</p>
-                </div>
-                <div>
-                    <p>Aenean volutpat malesuada nunc, nec ullamcorper massa scelerisque vulputate. Integer sit amet orci vulputate, imperdiet libero at, efficitur lacus. Maecenas tristique leo in massa faucibus, vitae molestie augue auctor.</p>
-                    <p>- Zion Ryder</p>
-                </div>
+                <Reveal>
+                    <div>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi varius, turpis vitae molestie sodales, ligula sapien scelerisque nunc, in fermentum risus velit quis est. Donec gravida lorem felis, ut pellentesque leo luctus eu. Praesent sit amet ultricies sem. Praesent sit amet viverra nunc."</p>
+                        <p>- Eve Salinas</p>
+                    </div>
+                </Reveal>
+                <Reveal delay={400}>
+                    <div>
+                        <p>"Etiam lacinia in massa id malesuada. Fusce vitae ipsum egestas, pellentesque leo ac, malesuada augue. Sed id dictum est. Curabitur vitae magna congue, porttitor tellus quis, ullamcorper arcu. Curabitur ornare euismod vulputate. Praesent auctor ullamcorper dolor, vel malesuada nunc sollicitudin at."</p>
+                        <p>- Daria Farrington</p>
+                    </div>
+                </Reveal>
+                <Reveal delay={800}>
+                    <div>
+                        <p>Aenean volutpat malesuada nunc, nec ullamcorper massa scelerisque vulputate. Integer sit amet orci vulputate, imperdiet libero at, efficitur lacus. Maecenas tristique leo in massa faucibus, vitae molestie augue auctor.</p>
+                        <p>- Zion Ryder</p>
+                    </div>
+                </Reveal>
             </div>
         </div>
     )
