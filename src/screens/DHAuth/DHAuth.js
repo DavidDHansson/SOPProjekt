76db3bd993d58d8723f9bc37b9d24bdac7ee9d6f
David Hansson <Hansson.d.david@gmail.com>
2020-11-29 14:43:39 +0100

    Api logic

--- src/screens/DHAuth/DHAuth.js
+++ src/screens/DHAuth/DHAuth.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./DHAuth.css";
 
 export default function DHAuth() {
 
+    const [password, setPassword] = useState();
+    const [email, setEmail] = useState();
+
     function login(e) {
         e.preventDefault();
+
+        fetch("https://4hansson.dk/api/sop/login.php", {
+            method: "POST",
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.log(err));
     }
 
     return (
@@ -12,9 +28,9 @@ export default function DHAuth() {
             <div className="dhauth-ticket-wrapper">
                 <div className="dhauth-title">Custom Auth</div>
                 <form onSubmit={login} className="dhauth-form-wrapper">
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="submit" className="dhauth-submit" value="Login!"/>
+                    <input type="email" placeholder="Email" onInput={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" onInput={e => setPassword(e.target.value)} />
+                    <input type="submit" className="dhauth-submit" value="Login!" />
                     <span>Sign up</span>
                 </form>
             </div>
