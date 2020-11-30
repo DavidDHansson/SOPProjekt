69766e23989e3a1bdbdf4ecc8fe4a8a290512268
David Hansson <Hansson.d.david@gmail.com>
2020-11-30 11:13:53 +0100

    Same page mobile fix

--- src/screens/user/User.js
+++ src/screens/user/User.js
@@ -11,7 +11,7 @@ export default function User() {
     const tickets = [
         { type: "firebase", title: "Firebase", message: "Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more." },
         { type: "auth0", title: "Auth0", message: "Authenticate users across all of your applications with your own custom, secure, and standards-based unified login. Supports logins with providers like Facebook, Apple, Github and more with multi-factor authentication" },
-        { type: "custom", title: "PHP/MySQL", message: "Skræddersyet API'er og database til administration af brugere. Der er fokus på sikkerhed, kryptering, brugervenlighed og hastighed" }
+        { type: "custom", title: "PHP", message: "Skræddersyet API'er og database til administration af brugere. Der er fokus på sikkerhed, kryptering, brugervenlighed og hastighed" }
     ]
 
     return (
